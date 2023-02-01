7e214e47da2beec3a862e0d16d77af89e0999bcd
ChongHinYu <mailtohin274@hotmail.com>
2023-02-01 22:07:11 +0000

    Highlight feature and centered content

--- index.js
+++ index.js
@@ -7,31 +7,37 @@ let awayText = document.getElementById("Away")
 function addHome1Point() {
     homeScore += 1
     homeText.textContent = homeScore
+    highlightBox()
 }
 
 function addHome2Points() {
     homeScore += 2
     homeText.textContent = homeScore
+    highlightBox()
 }
 
 function addHome3Points() {
     homeScore += 3
     homeText.textContent = homeScore
+    highlightBox()
 }
 
 function addAway1Point() {
     awayScore += 1
     awayText.textContent = awayScore
+    highlightBox()
 }
 
 function addAway2Points() {
     awayScore += 2
     awayText.textContent = awayScore
+    highlightBox()
 }
 
 function addAway3Points() {
     awayScore += 3
     awayText.textContent = awayScore
+    highlightBox()
 }
 
 function newGame() {
@@ -39,4 +45,19 @@ function newGame() {
     homeText.textContent = homeScore
     awayScore = 0
     awayText.textContent = awayScore
+    highlightBox()
+}
+
+function highlightBox(){
+    if(homeScore > awayScore){
+        homeText.classList.add("highlight")
+        awayText.classList.remove("highlight")
+    } else if (homeScore < awayScore) {
+        awayText.classList.add("highlight")
+        homeText.classList.remove("highlight")
+    } else {
+        awayText.classList.remove("highlight")
+        homeText.classList.remove("highlight")
+    }
+
 }
